7b786365e6c93b616226113c0383810507892c1c
VladislavShults <shvs93@mail.ru>
2022-11-11 18:44:29 +0500

    fix math.ceil

--- src/features/users/application/users.servive.ts
+++ src/features/users/application/users.servive.ts
@@ -3,7 +3,6 @@ import { UsersRepository } from '../infrastructure/users.repository';
 import { ObjectId } from 'mongodb';
 import { CreateUserDto } from '../api/models/create-user.dto';
 import { v4 as uuidv4 } from 'uuid';
-import add from 'date-fns/add';
 import { UserDBType } from '../types/users.types';
 
 @Injectable()
@@ -26,10 +25,11 @@ export class UsersService {
       },
       emailConfirmation: {
         confirmationCode: uuidv4(),
-        expirationDate: add(new Date(), { hours: 5 }),
+        expirationDate: new Date(new Date().getHours() + 5),
         isConfirmed: false,
       },
     };
+    console.log(user.emailConfirmation.isConfirmed);
     await this.usersRepository.createUser(user);
     return user._id;
   }
