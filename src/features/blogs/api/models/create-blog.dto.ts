b7da1861b901bacd0809145c4d11224e50fb3c10
VladislavShults <shvs93@mail.ru>
2022-11-19 15:37:44 +0500

    fix blog type

--- src/features/blogs/api/models/create-blog.dto.ts
+++ src/features/blogs/api/models/create-blog.dto.ts
@@ -2,11 +2,11 @@ import { IsString, IsUrl, Length, MaxLength } from 'class-validator';
 
 export class CreateBlogDto {
   @IsString()
-  @Length(0, 15)
+  @Length(3, 15)
   name: string;
 
   @IsString()
-  @MaxLength(500)
+  @Length(3, 500)
   description: string;
 
   @IsUrl()
