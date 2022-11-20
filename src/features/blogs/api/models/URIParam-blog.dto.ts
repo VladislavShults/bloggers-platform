68996f3350e47b8e281952c1ed5e0ff71c0cf308
VladislavShults <shvs93@mail.ru>
2022-11-20 11:51:11 +0500

    custom validation blogId

--- src/features/blogs/api/models/URIParam-blog.dto.ts
+++ src/features/blogs/api/models/URIParam-blog.dto.ts
@@ -1,7 +1,3 @@
-import { Validate } from 'class-validator';
-import { BlogIdValidation } from '../../../posts/validation/blogId-validation';
-
 export class URIParamBlogDto {
-  // @Validate(BlogIdValidation)
   blogId: string;
 }
