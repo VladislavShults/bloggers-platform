b8c4f2be85f43614cd3124e8e72d9763005e9c10
VladislavShults <shvs93@mail.ru>
2022-11-21 10:32:32 +0500

    check posts by blogId guard

---
+++ src/features/blogs/guards/check-posts-by-blog-id-in-d-b.service.ts
@@ -0,0 +1,29 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Model } from 'mongoose';
+import { PostDBType } from '../../posts/types/posts.types';
+
+@Injectable()
+export class CheckPostsByBlogIdInDB implements CanActivate {
+  constructor(
+    @Inject('POST_MODEL')
+    private readonly postModel: Model<PostDBType>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const params = request.params;
+    if (params.blogId.length !== 24)
+      throw new HttpException('POSTS NOT FOUND', HttpStatus.NOT_FOUND);
+    const post = await this.postModel.find({ blogId: params.blogId });
+    if (!post) throw new HttpException('POSTS NOT FOUND', HttpStatus.NOT_FOUND);
+    return true;
+  }
+}
